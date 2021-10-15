74e7d4cbe6caa881e6d53c1a02bebf57bff15df8
yang09ch <1436933542@qq.com>
2021-10-15 23:27:28 +0800

    1

--- src/main/java/com/kgc/controller/AjaxController.java
+++ src/main/java/com/kgc/controller/AjaxController.java
@@ -2,7 +2,7 @@ package com.kgc.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-//ajax 控制类
+//ajax 控制类  所有异步操作在本控制类进行
 @RestController
 @RequestMapping("ajax")
 public class AjaxController {

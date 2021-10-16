8536721400b4d8f1618d78e8330c56aedd28304e
xzh <2273796167@qq.com>
2021-10-16 17:33:44 +0800

    User表个别接口

--- src/main/java/com/kgc/mapper/UserMapper.java
+++ src/main/java/com/kgc/mapper/UserMapper.java
@@ -4,6 +4,8 @@ import com.kgc.pojo.User;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
+import java.util.List;
+
 @Mapper
 public interface UserMapper {
     /**
@@ -13,4 +15,17 @@ public interface UserMapper {
      * @return
      */
     User getUserLogin(@Param("userName") String userName,@Param("userPassword") String userPassword);
+      //计算分页总页数
+    Integer getCount();
+        //分页查询根据名字查询，根据userGender查询
+       List<User> SelectUser(@Param("userNickName") String userNickName,
+                             @Param("userGender") Integer userGender,
+                             @Param("pageIndex") Integer pageIndex,
+                             @Param("pageSize") Integer pageSize);
+           //根据userid查找
+         User getByidUser(Integer userId);
+           //新增用户表
+           Integer add(User user);
+            //注销用户表
+           Integer delete(Integer userId);
 }
